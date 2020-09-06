b07f9199fbae1a618399b510db747131260c5d0e
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-09-06 20:14:01 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,56 @@
+class Song
+
+  attr_accessor :name 
+  attr_reader :artist, :genre
+
+  @@all = []
+
+  def initialize(name,artist=nil,genre=nil)
+    @name = name
+    self.artist = artist if artist
+    self.genre = genre if genre
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    song = self.new(name)
+    song.save
+    song
+  end
+
+  def artist=(art_obj)
+    @artist = art_obj
+    art_obj.add_song(self)
+  end
+
+  def genre=(genre)
+    @genre= genre
+    if !(genre.songs.include?(self))
+    genre.songs << self
+  end
+  end
+
+  def self.find_by_name(name)
+    self.all.find { |song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+
+  def self.create_by_name(name)
+    create(name)
+  end
+  
+end
