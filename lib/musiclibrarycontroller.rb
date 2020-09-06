222682b4248ab60d3ee750a09656e12d47158854
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-09-06 21:37:46 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -41,7 +41,7 @@ class MusicLibraryController
         song.name
       end
     new_list.each do |song_name|
-      puts "#{counter}. #{song_name.artist.name}"
+      puts "#{counter}. #{song_name.name}"
       counter += 1
     end
   end
