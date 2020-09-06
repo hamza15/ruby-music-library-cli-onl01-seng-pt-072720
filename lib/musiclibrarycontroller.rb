c5f44e7ac2f66965feaf7d8f3bd5c1a04063c4df
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-09-06 21:18:39 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -2,7 +2,7 @@ class MusicLibraryController
   
   attr_accessor :path
   
-  def initialize(path="./db/mp3s/")
+  def initialize(path="./db/mp3s")
     importer = MusicImporter.new(path)
     importer.import
   end
