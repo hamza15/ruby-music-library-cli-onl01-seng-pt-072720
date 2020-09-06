51e9dd75d5fa422dae8a5e1494d645d2faf0f2be
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-09-06 21:40:48 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -58,7 +58,7 @@ class MusicLibraryController
   end
   
   def list_songs_by_artist
-    Please enter the name of an artist:
+    puts "Please enter the name of an artist:"
     user_input = gets.chomp
     
   end
